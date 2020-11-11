af407d97d269b992a2050960b09f033a7e516548
Fanni Nagy <nfanni97@gmail.com>
2020-11-11 15:25:01 +0100

    Apparently, the last commit was already the solved state, except for Main

--- 09_Adapter_Interpreter_DependencyInjection_SpaceBasedSOA/Lab9_adapter/src/dp/Main.java
+++ 09_Adapter_Interpreter_DependencyInjection_SpaceBasedSOA/Lab9_adapter/src/dp/Main.java
@@ -3,20 +3,13 @@ package dp;
 public class Main {
 
 	public static void main(String[] args) {
-		Object[] shapes = 
-	        {
-	            new LegacyLine(), new LegacyRectangle()
-	        };
-	        // A begin and end point from a graphical editor
-	        int x1 = 10, y1 = 20;
-	        int x2 = 30, y2 = 60;
-	        for (int i = 0; i < shapes.length; ++i){
-	        	if (shapes[i] instanceof LegacyLine)
-		            ((LegacyLine)shapes[i]).draw(x1, y1, x2, y2);
-		          else if (shapes[i] instanceof LegacyRectangle)
-		            ((LegacyRectangle)shapes[i]).draw(Math.min(x1, x2), Math.min(y1, y2)
-		              , Math.abs(x2 - x1), Math.abs(y2 - y1));
-	        }
+		NewTarget[] shapes = { new LineAdapter(new LegacyLine()), new RectangleAdapter(new LegacyRectangle()) };
+		// A begin and end point from a graphical editor
+		int x1 = 10, y1 = 20;
+		int x2 = 30, y2 = 60;
+		for (int i = 0; i < shapes.length; ++i) {
+			shapes[i].draw(x1, y1, x2, y2);
+		}
 	}
 
 }
